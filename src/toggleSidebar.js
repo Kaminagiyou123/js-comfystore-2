9e16a6293c6046b9514bd7728ab96456eb9b1c1f
Ran You <ran.you8311@gmail.com>
2021-03-24 18:19:06 -0700

    sidebar complete

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,1 +1,13 @@
-import { getElement } from './utils.js';
+import { getElement } from "./utils.js";
+
+const toggleNav = getElement(".toggle-nav");
+const sideBar = getElement(".sidebar-overlay");
+const closeBtn = getElement(".sidebar-close");
+
+toggleNav.addEventListener("click", () => {
+  sideBar.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+  sideBar.classList.remove("show");
+});
