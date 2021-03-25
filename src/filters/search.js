cd86cb7a2ead4cdd4366d5a75e079a9b9e05b854
Ran You <ran.you8311@gmail.com>
2021-03-24 22:34:48 -0700

    search filter complete

--- src/filters/search.js
+++ src/filters/search.js
@@ -1,5 +1,27 @@
-import { getElement } from '../utils.js';
-import display from '../displayProducts.js';
-const setupSearch = () => {};
+import { getElement } from "../utils.js";
+import display from "../displayProducts.js";
+const setupSearch = (store) => {
+  const form = getElement(".input-form");
+  const nameInput = getElement(".search-input");
+
+  form.addEventListener("keyup", () => {
+    const value = nameInput.value;
+    if (value) {
+      const newStore = store.filter((item) => {
+        if (item.name.toLowerCase().startsWith(value)) {
+          return item;
+        }
+      });
+      if (newStore.length > 0) {
+        display(newStore, getElement(".products-container"));
+      } else {
+        const products = getElement(".products-container");
+        products.innerHTML = `<h3 class='filter-error'>sorry, no product matched your search</h3>`;
+      }
+    } else {
+      display(store, getElement(".products-container"));
+    }
+  });
+};
 
 export default setupSearch;
