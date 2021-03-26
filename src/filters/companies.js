a410e7246ab599d14e65d0fdf102ee60936c5805
Ran You <ran.you8311@gmail.com>
2021-03-25 18:36:07 -0700

    company filter complete

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -1,6 +1,25 @@
 import { getElement } from "../utils.js";
 import display from "../displayProducts.js";
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
 
-const setupCompanies = (store) => {};
+const setupCompanies = (store) => {
+  let companies = store.map((item) => item.company);
+  companies = ["All", ...companies.filter(onlyUnique)];
+  const companiesDOM = getElement(".companies");
+  companiesDOM.innerHTML = companies
+    .map((company) => {
+      return ` <button class="company-btn">${company}</button>`;
+    })
+    .join("");
+
+  companiesDOM.addEventListener("click", (e) => {
+    const company = e.target.textContent;
+
+    const newStore = store.filter((item) => item.company === company);
+    display(newStore, getElement(".products-container"));
+  });
+};
 
 export default setupCompanies;
